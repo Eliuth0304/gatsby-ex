fa85370422b514ae6853c190d6fd7265513ba2ab
akramansari1433 <akramansari1433@gmail.com>
2023-01-03 17:39:39 +0530

    added delte user feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,9 +57,35 @@ const IndexPage = () => {
       event.target.reset();
    };
 
+   const handleDelete = async (id) => {
+      const query = `mutation insertUser($id:Int!){
+        delete_users(where: {id: {_eq: $id}}){
+          affected_rows
+        }
+      }`;
+      const response = await fetch(
+         "https://major-honeybee-52.hasura.app/v1/graphql",
+         {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+               query,
+               variables: { id },
+            }),
+         }
+      );
+
+      const res = await response.json();
+      res.data && alert("User Deleted");
+   };
+
    return (
       <div>
-         <h1 className="text-4xl text-center p-3"> Gatsby Husura App </h1>
+         <h1 className="text-4xl text-center p-3 underline ">
+            Gatsby Husura App
+         </h1>
          <form
             className=" flex justify-center mx-2 my-5"
             onSubmit={handleSubmit}
@@ -79,10 +105,11 @@ const IndexPage = () => {
                required
             />
             <button type="submit" className="bg-blue-500 rounded p-1 mx-2">
-               Submit
+               Add User
             </button>
          </form>
          <div className="m-2">
+            <h1 className="font-bold text-2xl my-3 text-center">User's List</h1>
             <table className="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
                <tbody className="text-xs text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
@@ -97,6 +124,9 @@ const IndexPage = () => {
                      </th>
                      <th scope="col" className="py-3 px-6">
                         Creadted At
+                     </th>
+                     <th scope="col" className="py-3 px-6">
+                        Delete
                      </th>
                   </tr>
                   {response.data?.data.users.map((user) => (
@@ -115,6 +145,14 @@ const IndexPage = () => {
                         </th>
                         <th scope="col" className="py-3 px-6">
                            {user.created_at}
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                           <button
+                              className="p-2 bg-red-400 rounded"
+                              onClick={() => handleDelete(user.id)}
+                           >
+                              Delete
+                           </button>
                         </th>
                      </tr>
                   ))}
