f54c1206ba27653e1720e9e3a1368dff6c81ec02
akramansari1433 <akramansari1433@gmail.com>
2023-01-03 17:11:44 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,19 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `gatsby-hasura-app`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+   siteMetadata: {
+      title: `gatsby-hasura-app`,
+      siteUrl: `https://www.yourdomain.tld`,
+   },
+   plugins: [
+      {
+         resolve: "gatsby-source-graphql",
+         options: {
+            typeName: "HASURA",
+            fieldName: "hasura",
+            url: "https://major-honeybee-52.hasura.app/v1/graphql",
+         },
+      },
+      "gatsby-plugin-postcss",
+   ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,130 @@
-import * as React from "react"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import React, { useState } from "react";
+import "../styles/global.css";
+import { useQuery } from "@tanstack/react-query";
 
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+   const [name, setName] = useState();
+   const [email, setEmail] = useState();
 
-export default IndexPage
+   const response = useQuery(["response"], async () => {
+      const query = `query getUser{
+      users{
+        id
+        name
+        email
+        created_at
+      }
+    }`;
+      return await (
+         await fetch("https://major-honeybee-52.hasura.app/v1/graphql", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+               query,
+            }),
+         })
+      ).json();
+   });
 
-export const Head = () => <title>Home Page</title>
+   const handleSubmit = async (event) => {
+      event.preventDefault();
+      const query = `mutation insertUser($name:String!,$email:String!){
+        insert_users_one(object:{
+          name:$name,
+          email:$email
+        }){
+          id
+        }
+      }`;
+      const response = await fetch(
+         "https://major-honeybee-52.hasura.app/v1/graphql",
+         {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+               query,
+               variables: { name, email },
+            }),
+         }
+      );
+
+      const res = await response.json();
+      res.data && alert("User Added");
+      event.target.reset();
+   };
+
+   return (
+      <div>
+         <h1 className="text-4xl text-center p-3"> Gatsby Husura App </h1>
+         <form
+            className=" flex justify-center mx-2 my-5"
+            onSubmit={handleSubmit}
+         >
+            <input
+               type="text"
+               className="rounded border-2 border-blue-500 p-1 mx-1"
+               placeholder="Name"
+               onChange={(e) => setName(e.target.value)}
+               required
+            />
+            <input
+               type="text"
+               className="rounded border-2 border-blue-500 p-1 mx-1"
+               placeholder="Email"
+               onChange={(e) => setEmail(e.target.value)}
+               required
+            />
+            <button type="submit" className="bg-blue-500 rounded p-1 mx-2">
+               Submit
+            </button>
+         </form>
+         <div className="m-2">
+            <table className="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
+               <tbody className="text-xs text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                     <th scope="col" className="py-3 px-6">
+                        Id
+                     </th>
+                     <th scope="col" className="py-3 px-6">
+                        Name
+                     </th>
+                     <th scope="col" className="py-3 px-6">
+                        Email
+                     </th>
+                     <th scope="col" className="py-3 px-6">
+                        Creadted At
+                     </th>
+                  </tr>
+                  {response.data?.data.users.map((user) => (
+                     <tr
+                        key={user.id}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                     >
+                        <th scope="col" className="py-3 px-6">
+                           {user.id}
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                           {user.name}
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                           {user.email}
+                        </th>
+                        <th scope="col" className="py-3 px-6">
+                           {user.created_at}
+                        </th>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
+         </div>
+      </div>
+   );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
